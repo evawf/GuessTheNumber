9c52971a812aba9960dc6135f5ddffb173ac0c89
eva.wang <eva.wang81@gmail.com>
2021-06-15 08:53:19 +0800

    Added guess function(0-10)

--- app.js
+++ app.js
@@ -1,2 +1,29 @@
-const InputNumber = document.getElementById('InputNumber').nodeValue;
-console.log(InputNumber);
+let InputNumber;
+const theAnswer = Math.floor(Math.random() * 11);
+console.log(theAnswer);
+
+function getNumber(){
+    // Selecting the input element and get its value 
+    const guessedNumber = document.getElementById("InputNumber").value;
+    // Displaying the value
+    console.log(guessedNumber);
+    let InputNumber = guessedNumber;
+
+    const showResult = document.getElementById('showResult');
+    const Result = document.createElement('div');
+
+    if (theAnswer < InputNumber) {
+        Result.innerHTML = `${InputNumber} is bigger than the answer!`
+        showResult.appendChild(Result);
+    }
+
+    if (theAnswer > InputNumber) {
+        Result.innerHTML = `${InputNumber} is smaller than the answer!`
+        showResult.appendChild(Result);
+    }
+
+    if (theAnswer == InputNumber) {
+        Result.innerHTML = `Bingo!!! You got the correct number!`
+        showResult.appendChild(Result);
+    }
+}
